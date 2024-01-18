153ceebc803ce396d5422bff645e65e7da7026f9
Yoojin Lee <yoojinLee.dev@gmail.com>
2024-01-18 22:31:06 +0900

    feat: Add common self validating class

--- src/main/java/com/example/hexagonal/common/SelfValidating.java
+++ src/main/java/com/example/hexagonal/common/SelfValidating.java
@@ -1,5 +1,20 @@
 package com.example.hexagonal.common;
 
 
+import jakarta.validation.*;
+
+import java.util.Set;
+
 public abstract class SelfValidating<T> {
+    private Validator validator;
+
+    protected SelfValidating() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    protected void validateSelf() {
+        Set<ConstraintViolation<T>> violations = validator.validate((T) this);
+        if(violations.isEmpty()) throw new ConstraintViolationException(violations);
+    }
 }
