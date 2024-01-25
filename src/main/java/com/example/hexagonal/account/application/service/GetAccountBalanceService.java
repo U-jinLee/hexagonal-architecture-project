9391097f87b1fb6a42a1eaeceb9b16dc596e70ad
Yoojin Lee <yoojinLee.dev@gmail.com>
2024-01-25 22:22:12 +0900

    feat: Make GetAccountBalanceQuery service

--- src/main/java/com/example/hexagonal/account/application/service/GetAccountBalanceService.java
+++ src/main/java/com/example/hexagonal/account/application/service/GetAccountBalanceService.java
@@ -1,15 +1,20 @@
 package com.example.hexagonal.account.application.service;
 
 import com.example.hexagonal.account.application.port.in.GetAccountBalanceQuery;
+import com.example.hexagonal.account.application.port.out.LoadAccountPort;
 import com.example.hexagonal.account.domain.Account;
 import com.example.hexagonal.account.domain.Money;
 import lombok.RequiredArgsConstructor;
 
+import java.time.LocalDateTime;
+
 @RequiredArgsConstructor
 public class GetAccountBalanceService implements GetAccountBalanceQuery {
 
+	private final LoadAccountPort loadAccountPort;
 	@Override
 	public Money getAccountBalance(Account.AccountId accountId) {
-		return null;
+		return loadAccountPort.loadAccount(accountId, LocalDateTime.now())
+				.calculateBalance();
 	}
 }
